70e2a620e871f02b4e58c2fcbb79932544f8d335
javiersantibanez <javier.santibanez.93@gmail.com>
2016-06-28 14:30:59 -0400

    Capa Vista Lista

---
+++ src/Capa_Vista/ProyectoFinalv1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Capa_Vista;
+
+/**
+ *
+ * @author Javier Santibañez
+ */
+public class ProyectoFinalv1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vista_Principal a = new Vista_Principal();
+        a.setVisible(true);
+    }
+    
+}
